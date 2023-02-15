4f40d4ef92e8f44d8c5fefc7187373b3f0cd84d6
windows 10 <dimpulsunayana205@gmail.com>
2023-02-16 00:11:59 +0530

    updated user repo

--- vars/lib.groovy
+++ vars/lib.groovy
@@ -25,8 +25,16 @@ def call(){
                 }
 
                 stage('Quality Control') {
+                    environment{
+                        Sonar_User= '$(aws ssm get -parameters --region us-east-1 --names sonarqube.user --with-decryption ' +
+                                '-- query Parameters[0].Value | sed \'s/"//g\')'
+                        Sonar_Pass= '$(aws ssm get -parameters --region us-east-1 --names sonarqube.pass --with-decryption ' +
+                                '-- query Parameters[0].Value | sed \'s/"//g\')'
+                            // some block
+                        }
                     steps {
-                        echo 'Quality Control'
+
+                        sh "sonar-scanner -Dsonar.host.url=http://172.31.13.153:9000 -Dsonar.login=${Sonar_User} -Dsonar.password=${Sonar_Pass} -Dsonar.projectKey=cart"
                     }
                 }
 
