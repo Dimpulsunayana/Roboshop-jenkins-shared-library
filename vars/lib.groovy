bcbdf3e9051141de0d5727bae7153a1329b0a376
windows 10 <dimpulsunayana205@gmail.com>
2023-02-16 15:35:18 +0530

    updated user repo

--- vars/lib.groovy
+++ vars/lib.groovy
@@ -33,11 +33,11 @@ def call(){
                             // some block
                         }
                     steps {
-                        script {
+                        node {
                             wrap([$class: 'MaskPasswordsBuildWrapper',
                                   varPasswordPairs: [[password: '{Sonar_Pass}',user: '{Sonar_User}', var: 'SECRET']]]) {
-                                x = sh "echo password = ${Sonar_Pass}"
-                                sh "sonar-scanner -Dsonar.host.url=http://172.31.13.153:9000 -Dsonar.login=${Sonar_User} -Dsonar.password=${x} -Dsonar.projectKey=cart"
+                                sh "echo password = ${Sonar_Pass}"
+                                sh "sonar-scanner -Dsonar.host.url=http://172.31.13.153:9000 -Dsonar.login=${Sonar_User} -Dsonar.password=${Sonar_Pass} -Dsonar.projectKey=cart"
                             }
 
                         }
