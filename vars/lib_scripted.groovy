c94caf55e029627606e76fd39611ebda5e7d5488
windows 10 <dimpulsunayana205@gmail.com>
2023-02-17 22:47:11 +0530

    updated user repo

--- vars/lib_scripted.groovy
+++ vars/lib_scripted.groovy
@@ -30,8 +30,8 @@ def call(){
                         '--query Parameters[0].Value | sed \'s/"//g\')'
                 wrap([$class          : 'MaskPasswordsBuildWrapper',
                       varPasswordPairs: [[password: '{Sonar_Pass}', user: '{Sonar_User}', var: 'SECRET']]]) {
-                    sh "sonar-scanner -Dsonar.host.url=http://172.31.13.153:9000 -Dsonar.password=${Sonar_Pass} " +
-                            "-Dsonar.login=${Sonar_User} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
+                  //  sh "sonar-scanner -Dsonar.host.url=http://172.31.13.153:9000 -Dsonar.password=${Sonar_Pass} " +
+                    //        "-Dsonar.login=${Sonar_User} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
                 }
             }
             if(env.PUSH_CODE == "true") {
