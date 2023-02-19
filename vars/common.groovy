4245e7d548edc01c87345e3bdec7ed6eb9c21b17
windows 10 <dimpulsunayana205@gmail.com>
2023-02-19 15:54:18 +0530

    updated user repo

--- vars/common.groovy
+++ vars/common.groovy
@@ -35,7 +35,7 @@ def email(email_note){
 }
 def artifactPush(){
     if (app_lang== "nodejs"){
-    sh "zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION"
+    sh "zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION ${extrafiles}"
 }
     sh 'ls -l'
     sh "curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://172.31.12.68:8081/repository/${component}/${component}-${TAG_NAME}.zip"
